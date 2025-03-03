8a2b5b3d7e5d5245e225bfc73504d36e13ed5f82
mathuzin <moplgalvao@furb.br>
2025-03-03 14:11:41 -0300

    Adicionando todos os arquivos do projeto

---
+++ src/main/java/com/loiane/controller/CourseController.java
@@ -0,0 +1,26 @@
+package com.loiane.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.loiane.model.Course;
+import com.loiane.repository.CourseRepository;
+
+@RestController
+@RequestMapping("/api/courses")
+public class CourseController {
+    private final CourseRepository courseRepository;
+
+    public CourseController(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
+
+    @GetMapping
+    public List<Course> list() {
+        return courseRepository.findAll();
+    }
+
+}
